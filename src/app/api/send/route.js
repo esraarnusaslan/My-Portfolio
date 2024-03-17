e31c2fade10702111a487414429e82d71891aa32
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-17 22:04:13 +0300

    ""

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -4,6 +4,8 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
+
+//
 export async function POST(req, res) {
     const { email, subject, message } = await req.json();
     console.log(email, subject, message);
