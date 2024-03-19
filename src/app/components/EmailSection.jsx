7081e527531723ad2a3452472b31110c0e0e6f43
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-19 21:14:27 +0300

    "emailsection updated with fromspark "

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,40 +1,11 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import GithubIcon from '../../../public/github-icon.svg';
 import LinkedinIcon from '../../../public/linkedin-icon.svg';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const EmailSection = () => {
-    const [emailSubmitted, setEmailSubmitted] = useState(false);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
-        };
-        const JSONdata = JSON.stringify(data);
-        const endpoint = '/api/send';
-
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSONdata,
-        };
-
-        const response = await fetch(endpoint, options);
-        const resData = await response.json();
-
-        if (response.status === 200) {
-            console.log('Message sent.');
-            setEmailSubmitted(true);
-        }
-    };
-
     return (
         <section
             id="contact"
@@ -65,66 +36,65 @@ const EmailSection = () => {
                 </div>
             </div>
             <div>
-                {emailSubmitted ? (
-                    <p className="text-green-500 text-sm mt-2">
-                        Email sent successfully!
-                    </p>
-                ) : (
-                    <form className="flex flex-col" onSubmit={handleSubmit}>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="email"
-                                className="text-white block mb-2 text-sm font-medium"
-                            >
-                                Your email
-                            </label>
-                            <input
-                                name="email"
-                                type="email"
-                                id="email"
-                                required
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="jacob@google.com"
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="subject"
-                                className="text-white block text-sm mb-2 font-medium"
-                            >
-                                Subject
-                            </label>
-                            <input
-                                name="subject"
-                                type="text"
-                                id="subject"
-                                required
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="Just saying hi"
-                            />
-                        </div>
-                        <div className="mb-6">
-                            <label
-                                htmlFor="message"
-                                className="text-white block text-sm mb-2 font-medium"
-                            >
-                                Message
-                            </label>
-                            <textarea
-                                name="message"
-                                id="message"
-                                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                placeholder="Let's talk about..."
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="bg-gradient-to-br from-secondary-500 via-primary-500 to-blue-500 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                <form
+                    method="post"
+                    action="https://submit-form.com/9nR3CMYLk"
+                    target={'_blank'}
+                    className="flex flex-col"
+                >
+                    <div className="mb-6">
+                        <label
+                            htmlFor="email"
+                            className="text-white block mb-2 text-sm font-medium"
                         >
-                            Send Message
-                        </button>
-                    </form>
-                )}
+                            Your email
+                        </label>
+                        <input
+                            name="email"
+                            type="email"
+                            id="email"
+                            required
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="jacob@google.com"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="subject"
+                            className="text-white block text-sm mb-2 font-medium"
+                        >
+                            Subject
+                        </label>
+                        <input
+                            name="subject"
+                            type="text"
+                            id="subject"
+                            required
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="Just saying hi"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="message"
+                            className="text-white block text-sm mb-2 font-medium"
+                        >
+                            Message
+                        </label>
+                        <textarea
+                            name="message"
+                            id="message"
+                            className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                            placeholder="Let's talk about..."
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-gradient-to-br from-secondary-500 via-primary-500 to-blue-500 text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                        Send Message
+                    </button>
+                </form>
             </div>
         </section>
     );
